f5c5e39a6b2c6b4e955a6bad965eb6ddee9003d1
linf <lblinf.dk@foxmail.com>
2025-07-25 18:22:46 +0800

    新增 .nvmrc 文件以指定 Node.js 版本，新增 start.sh 脚本以简化服务启动流程，更新 pnpm-lock.yaml 文件以反映依赖项的版本变更。

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+name="score-counter-service"
+
+echo -e "\033[34m\n\nUsing nvm...\033[0m"
+npx nvm use;
+
+echo -e "\033[34m\n\nInstalling web dependencies...\033[0m"
+cd ./web && npx pnpm i && pnpm build && cd ..;
+
+echo -e "\033[34m\n\nInstalling service dependencies...\033[0m"
+cd ./service && npx pnpm i && npx tsc && cd ..;
+
+echo -e "\033[34m\n\nStarting service...\033[0m"
+npx pm2 delete $name;
+npx pm2 start ./service/dist/main.js --name $name --interpreter $(which node)
